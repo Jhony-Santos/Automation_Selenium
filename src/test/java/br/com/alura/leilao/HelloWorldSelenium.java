f682196ca1ef8363e1afbb92e0ca3aeaa622e16c
jntd <jntd@gft.com>
2022-02-09 23:59:20 -0300

    classe reestruturada

--- src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
+++ src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -8,9 +8,11 @@ public class HelloWorldSelenium {
 
 	@Test
 	public void helloTest() {
+
 		System.setProperty("webdriver.chrome.driver", "C://Users//jntd//Downloads//chromedriver.exe");
 		WebDriver browser = new ChromeDriver();
 		browser.navigate().to("http://localhost:8080/leiloes");
+		browser.quit();
 
 	}
 
