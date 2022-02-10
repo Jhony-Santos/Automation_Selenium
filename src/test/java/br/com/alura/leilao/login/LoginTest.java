7230a2b5dbd0cf0ef0a30575ab4fe84edbe26d75
jntd <jntd@gft.com>
2022-02-10 00:40:29 -0300

    métdo para validar usuário inválido adicionado

--- src/test/java/br/com/alura/leilao/login/LoginTest.java
+++ src/test/java/br/com/alura/leilao/login/LoginTest.java
@@ -3,15 +3,17 @@ package br.com.alura.leilao.login;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 
+    String url = "http://localhost:8080/login";
+    String urlLoginInvalido="http://localhost:8080/login?error";
+
     @Test
     public void loginValido(){
-
-        String url = "http://localhost:8080/login";
 
         System.setProperty("webdriver.chrome.driver", "C://Users//jntd//Downloads//chromedriver.exe");
         WebDriver browser = new ChromeDriver();
@@ -24,7 +26,27 @@ public class LoginTest {
         Assert.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
 
         browser.quit();
+    }
+
+
+    @Test
+    public void loginInvalido(){
+
+        System.setProperty("webdriver.chrome.driver", "C://Users//jntd//Downloads//chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.navigate().to(url);
+        browser.findElement(By.id("username")).sendKeys("usuario-invalido");
+        browser.findElement(By.id("password")).sendKeys("senha-invalida");
+        browser.findElement(By.id("login-form")).submit();
+
+        Assert.assertTrue(browser.getCurrentUrl().equals(urlLoginInvalido));
+        Assert.assertTrue(browser.getPageSource().contains("Usuário e senha inválidos."));
+        Assert.assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("usuario-logado")));
+        String codePage=browser.getPageSource();
+        System.out.println(codePage);
+        browser.quit();
 
     }
+
 
 }
