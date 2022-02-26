f166c1ed2c41ec2573a0e02afb04056e640f0f97
jntd <jntd@gft.com>
2022-02-26 19:35:33 -0300

    RegisterAuctionsPage

--- src/test/java/br/com/alura/leilao/auctions/RegisterAuctionsPage.java
+++ src/test/java/br/com/alura/leilao/auctions/RegisterAuctionsPage.java
@@ -1,4 +1,17 @@
 package br.com.alura.leilao.auctions;
 
+import org.openqa.selenium.WebDriver;
+
 public class RegisterAuctionsPage {
+
+    private WebDriver browser;
+
+    public RegisterAuctionsPage(WebDriver browser){
+        this.browser=browser;
+    }
+
+    public void quit(){
+        this.browser.quit();
+    }
+
 }
