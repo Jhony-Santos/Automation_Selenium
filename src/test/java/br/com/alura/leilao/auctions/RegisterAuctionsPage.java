f4b65917d90d72552d4c58ee4b10abc3a31fe8c2
jntd <jntd@gft.com>
2022-03-01 18:42:03 -0300

    RegisterAuctionsPage

--- src/test/java/br/com/alura/leilao/auctions/RegisterAuctionsPage.java
+++ src/test/java/br/com/alura/leilao/auctions/RegisterAuctionsPage.java
@@ -1,5 +1,6 @@
 package br.com.alura.leilao.auctions;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class RegisterAuctionsPage {
@@ -10,8 +11,23 @@ public class RegisterAuctionsPage {
         this.browser=browser;
     }
 
+
+    public AuctionsPage registerAuctions(String name, String initialValue, String openingData)  {
+
+        this.browser.findElement(By.id("nome")).sendKeys(name);
+        this.browser.findElement(By.id("valorInicial")).sendKeys(initialValue);
+        this.browser.findElement(By.id("dataAbertura")).sendKeys(openingData);
+
+        this.browser.findElement(By.id("button-submit")).submit();
+
+        return new AuctionsPage(browser);
+
+    }
+
+
     public void quit(){
         this.browser.quit();
     }
+
 
 }
