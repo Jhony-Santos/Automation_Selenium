702d35c5bd2dc4bc238abb3cecc0aee2ad53f040
jntd <jntd@gft.com>
2022-03-01 18:41:10 -0300

    cadastrando um novo leilão

--- src/test/java/br/com/alura/leilao/auctions/AuctionsTest.java
+++ src/test/java/br/com/alura/leilao/auctions/AuctionsTest.java
@@ -2,39 +2,47 @@ package br.com.alura.leilao.auctions;
 
 import br.com.alura.leilao.login.LoginPage;
 import br.com.alura.leilao.login.LoginTest;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class AuctionsTest {
 
 
     private AuctionsPage auctionsPage;
 
-    @BeforeEach
-    public void beforeEach(){ }
 
 
     @AfterEach
     public void afterEach(){
-        auctionsPage.quit();
+        this.auctionsPage.quit();
     }
 
     @Test
-    public void registerAuctions() {
+    public void register() {
 
         LoginPage loginPage = new LoginPage();
-        //LoginTest loginTest=new LoginTest();
-        //loginTest.userValid();
         loginPage.fillLoginForm("fulano","pass");
         this.auctionsPage = loginPage.Login();
         auctionsPage.toChargeForm();
         RegisterAuctionsPage registerAuctionsPage = auctionsPage.toChargeForm();
 
+
+        String today= LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String name = "Leilão do dia " + today;
+        String value ="500.00";
+
+
+       this.auctionsPage = registerAuctionsPage.registerAuctions(name, value, today);
+       Assert.assertTrue(auctionsPage.isAuctionRegister(name,value, today));
+
+
+
     }
-
-
-
 
 
 
