0ef92bb93d83aae31e087efec0458ffcd9775e24
jntd <jntd@gft.com>
2022-03-01 18:39:17 -0300

    Page Object AuctionsPage

--- src/test/java/br/com/alura/leilao/auctions/AuctionsPage.java
+++ src/test/java/br/com/alura/leilao/auctions/AuctionsPage.java
@@ -1,16 +1,16 @@
 package br.com.alura.leilao.auctions;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class AuctionsPage {
 
   private static final String URL_REGISTER_ACTIONS="http://localhost:8080/leiloes/new";
-  private WebDriver browser;
+    private WebDriver browser;
 
   public AuctionsPage(WebDriver browser){
       this.browser = browser;
-      this.browser.navigate().to(URL_REGISTER_ACTIONS);
-
   }
 
 
@@ -18,10 +18,6 @@ public class AuctionsPage {
       this.browser.quit();
   }
 
-  public String codePage(){
-      return browser.getPageSource();
-
-  }
 
   public RegisterAuctionsPage toChargeForm(){
         this.browser.navigate().to(URL_REGISTER_ACTIONS);
@@ -29,6 +25,23 @@ public class AuctionsPage {
 
   }
 
+    public boolean isAuctionRegister(String name, String value, String data){
+
+      WebElement lineTableAuctions = this.browser.findElement(By.cssSelector("#table-auctions tbody tr:last-child"));
+
+      WebElement columnNome = lineTableAuctions.findElement(By.cssSelector("td:nth-child(1)"));
+      WebElement columnDataAbertura = lineTableAuctions.findElement(By.cssSelector("td:nth-child(2)"));
+      WebElement columnValorInicial = lineTableAuctions.findElement(By.cssSelector("td:nth-child(3)"));
+
+
+        // check the information
+
+        return columnNome.getText().equals(name)
+                && columnDataAbertura.getText().equals(data)
+                && columnValorInicial.getText().equals(value);
+
+
+    }
 
 
 }
